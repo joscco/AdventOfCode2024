6b7c71f74c9f303249a07229a8269871c2801d5a
Jonathan Schmitz <jomitz96@hotmail.com>
2024-12-10 09:41:33 +0100

    Make Day 10 solution a bit cleaner

--- src/day10/day10.py
+++ src/day10/day10.py
@@ -1,26 +1,24 @@
 with open("input.txt") as f:
-    field = [ list(line) for line in f.read().split("\n") ]
-
-    starts = [(j, i) for i, row in enumerate(field) for j, x in enumerate(row) if x == "0"]
+    field = [list(line) for line in f.read().split("\n")]
 
     def find_paths(field, value, pos, prev_path):
         if value == 9:
             return [prev_path]
 
-        neighbors = [(pos[0] + 1, pos[1]), (pos[0] - 1, pos[1]), (pos[0], pos[1] + 1), (pos[0], pos[1] - 1)]
-        neighbors_in_field = [n for n in neighbors if 0 <= n[0] < len(field[0]) and 0 <= n[1] < len(field)]
+        offsets = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        neighbors = [(pos[0] + dx, pos[1] + dy) for dx, dy in offsets]
+        valid_neighbors = [n for n in neighbors if 0 <= n[0] < len(field[0]) and 0 <= n[1] < len(field)]
         paths = []
-        for neighbor in neighbors_in_field:
+        for neighbor in valid_neighbors:
             if field[neighbor[1]][neighbor[0]] == str(value + 1):
                 paths.extend(find_paths(field, value + 1, neighbor, prev_path + [neighbor]))
         return paths
 
-    complete_paths = list()
-    for start in starts:
-        complete_paths += (find_paths(field, 0, start, [start]))
+    starts = [(j, i) for i, row in enumerate(field) for j, x in enumerate(row) if x == "0"]
+    complete_paths = [path for start in starts for path in find_paths(field, 0, start, [start])]
 
     # Take set of first and last pairs
-    pairs = set((path[0], path[-1]) for path in complete_paths)
+    pairs = {(path[0], path[-1]) for path in complete_paths}
     print(len(pairs))
 
     # Part 2

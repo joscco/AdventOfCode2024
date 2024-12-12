28965c1169863bad5a7cd7a837792f3a90b62b99
Jonathan Schmitz <jomitz96@hotmail.com>
2024-12-12 12:33:36 +0100

    Refactor Day 12 solution

--- src/day12/day12.py
+++ src/day12/day12.py
@@ -1,10 +1,11 @@
 with open("input.txt") as f:
     values = [list(line) for line in f.read().split("\n")]
 
-
     def get_neighborIndices(i, j):
         return [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
 
+    def is_in_grid(i, j):
+        return 0 <= i < len(values[0]) and 0 <= j < len(values)
 
     def find_component(i, j, visited_positions) -> set:
         component = set()
@@ -23,11 +24,9 @@ with open("input.txt") as f:
     def get_neighbors_of_same_value(i, j, value) -> list:
         result = []
         for neighborIndex in get_neighborIndices(i, j):
-            if 0 <= neighborIndex[1] < len(values) and 0 <= neighborIndex[0] < len(values[0]) and \
-                    values[neighborIndex[1]][neighborIndex[0]] == value:
+            if is_in_grid(*neighborIndex) and values[neighborIndex[1]][neighborIndex[0]] == value:
                 result.append(neighborIndex)
         return result
-
 
     def get_connected_components(grid):
         components = []
@@ -39,6 +38,7 @@ with open("input.txt") as f:
                 components.append(find_component(i, j, visited_positions))
         return components
 
+    connected_components = get_connected_components(values)
 
     def get_perimeter_length(component):
         perimeter = 0
@@ -50,15 +50,13 @@ with open("input.txt") as f:
             perimeter += stone_perimeter
         return perimeter
 
-
     def get_area(component):
         return len(component)
-
 
     def get_product(component):
         return get_area(component) * get_perimeter_length(component)
 
-    print(sum([get_product(component) for component in get_connected_components(values)]))
+    print(sum([get_product(component) for component in connected_components]))
 
     # Part 2
 
@@ -67,14 +65,11 @@ with open("input.txt") as f:
         seen = set(component)
         for n in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             for edge in sorted({(s[0] + n[0], s[1] + n[1]) for s in seen} - seen, key=lambda p: (p[0], p[1])):
-                exists = next((side for side in sides for p, d in side if
-                               abs(p[0] - edge[0]) + abs(p[1] - edge[1]) == 1 and d == n), None)
+                exists = next((side for side in sides for p, d in side if abs(p[0] - edge[0]) + abs(p[1] - edge[1]) == 1 and d == n), None)
                 exists.add((edge, n)) if exists else sides.append({(edge, n)})
         return len(sides)
-
 
     def get_product_2(component):
         return get_area(component) * get_number_of_sides(component)
 
-
-    print(sum([get_product_2(component) for component in get_connected_components(values)]))
+    print(sum([get_product_2(component) for component in connected_components]))
